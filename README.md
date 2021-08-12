dfb1aa16a87a74681a373c1e69f5a18ee94e7573
pcschreiber1 <pcschreiber1@gmail.com>
2021-08-12 15:18:09 +0200

    Creating simulation function with explicit SNR

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # Comp_Stat_Project
 Project for the course of Computation statistics, summer semester 2021 MSc Economics University of Bonn
+
+Content
+ - Variable selection with Lasso
+  - (Not really like bottom-up adding of variables to get optimal predictive model)
+ - Variable selection with Random Forest (RF)
+  - Why random forest and not boosting (who's penalty score looks similar to Lasso)
+  - Why comparing linear and non-linear method?
+    - common questions of interest like behaviour in collinearity?
+      - both seem to **"bet" on sparsity**? [not entirely sure for RF]
+    - or can they be competing methods? If so, for what type of applications
+      - maybe also interesting to simply compare - since the latter one isn't used much in economics?
+ - Simulation Study
+    - Measures: MSE, comparison to Oracle rate (OLS with true DGP)
+      - variable selection: retention frequency (Epprecht, 2013) [Genuer only use prediciton error]
+ - Application
+   
+Covariance matrix $\rho^{i-j}$ or additional term for correlation of only some variables?
+
+Genuer (2015): "RF is typically built using ntree = 2000 trees"
+
+Is RF model selection also a shrinkage/regularization method or also a subset selection method?
+
+Maybe not that important that they are the same method if they have other properties that connects the two: 
+ - RF is supposed to have low variance, no? So are shrinkage methods!
+    - So maybe the scenario where Lasso outperforms subset selection methods is also useful for RF? (Hastie et al 2020: Lasso outperforms forward step-wise selection if low signal-to-noise ratio (SNR))
+ - Variable Importance scores are biased towards correlated variables (Genuer, 2015 - citing Strobl)
+   - but not that helpful connection if both simply suffer from the same shortcoming (but maybe good for a side-note comparison)
+ - Are LASSO and RF both embedded feature selection methods?
---
+++ auxiliary_functions.R
@@ -0,0 +1,83 @@
+# This module contains auxiliary functions for XY
+# which are used in the main notebook.
+# --------------------------------------------------------
+
+# Imports
+library(MASS) 
+library(stats)
+
+# Generating Sample
+simulate <- function(n, #number of observations
+                     p, #number of covariates
+                     rho, #degree of covariance
+                     beta, #vetctor of true coefficients
+                     SNR # desired Signal-to-Noise ratio
+){
+  if (length(beta) != p){
+    cat("Number of beta coefficient unequal to p")
+  }else{
+    #Mean of explanatory variables
+    mu = rep(0,p) #all covariates are standardized with mean zero
+    
+    #Variance-Covariance Matrix
+    ###Note: Matrix only depends on p and rho
+    toep = rho^(0:(p-1)) #creates geometric series starting at one
+    Sigma = toeplitz(toep) #creates toeplitz matrix from geometric series: rho^(i-j)
+    
+    X = mvrnorm(n, mu, Sigma)
+    
+    # Set snr based on sample variance on infinitely large test set
+    var_mu = as.numeric(t(beta) %*% Sigma %*% beta)
+    sigma = sqrt(var_mu/SNR)
+    
+    
+    epsilon = rnorm(n)*sigma #, 0, sigma)
+    Y = X %*% beta + epsilon
+    
+    #-------Creating data frame
+    df <- data.frame(Y, X)
+    
+    list_1 = list("df" = df, "sigma" = sigma)
+    return(list_1)
+  } 
+}
+
+# Generate beta_1 vector
+beta_1 <- function(p, #number of covariates
+                   s  #degree of sparsity
+){
+  #--------------------------
+  # Beta vector with equally 
+  # spaced ones, the rest zeros
+  # -------------------------
+  beta = rep(0, p)
+  loc = round(seq(1,p, length.out=s))
+  beta[loc] = 1
+  return(beta)
+}
+
+# Generate beta_2 vector
+beta_2 <- function(p, #number of covariates
+                   s  #degree of sparsity
+){
+  #--------------------------
+  # Beta vector with ones at the first 
+  # entries, the rest zeros
+  # -------------------------
+  beta = c(rep(1,s),rep(0, p-s))
+  return(beta)
+}
+
+# Generate beta_3 vector
+beta_3 <- function(p, #number of covariates
+                   s, #degree of sparsity
+                   value  #value of coefficient
+){
+  #--------------------------
+  # Beta vector with weak sparsity:
+  # first s equal to ones, the rest
+  # geometric decay of values
+  # -------------------------
+  beta = c(rep(1,s), value^(1:(p-s))) 
+  return(beta)
+}
