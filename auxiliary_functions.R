591f1237b58b14cdc27fbb13b9ad5bafe1062f5a
pcschreiber1 <pcschreiber1@gmail.com>
2021-08-13 12:48:36 +0200

    First Simulation with RF

--- auxiliary_functions.R
+++ auxiliary_functions.R
@@ -5,6 +5,10 @@
 # Imports
 library(MASS) 
 library(stats)
+install.packages("glmnet")
+library(glmnet) #for LASSO
+install.packages("VSURF")
+library(VSURF)#for RF
 
 # Generating Sample
 simulate <- function(n, #number of observations
@@ -27,7 +31,7 @@ simulate <- function(n, #number of observations
     #explanatory variables
     X = mvrnorm(n, mu, Sigma)
     
-    # Set snr based on sample variance on infinitely large test set
+    # Set sigma based on sample variance on infinitely large test set
     var_mu = as.numeric(t(beta) %*% Sigma %*% beta)
     sigma = as.numeric(sqrt(var_mu/SNR))
     
@@ -81,3 +85,53 @@ beta_3 <- function(p, #number of covariates
   beta = c(rep(1,s), value^(1:(p-s))) 
   return(beta)
 }
+
+# Count retention of variables
+var_retention <- function(model_coef, #coefficients of the estimated model
+                          beta #true beta vector
+){
+  #--------------------------
+  # Counts how many variables were
+  # correctly identified by the estimated model
+  # -------------------------
+  non_zero = model_coef != 0 #find non-zero coefficients
+  non_zero = as.numeric(non_zero) #transform to numeric vector
+  if (length(model_coef) != length(beta)){
+    non_zero = as.numeric(non_zero)[-1] # exclude intercept placeholder for lasso!
+  }
+  
+  preserved = (non_zero + beta) == 2 # retention means double incidence
+  retention = sum(preserved) # preserved is boulian vector
+  return(retention)   
+}
+
+# Perform Cross-validated Lasso
+cv.lasso <- function(data, #data frame - dependent variable first
+                     beta # true coefficients
+){
+  #--------------------------
+  # Uses 10 fold CV and uses 1 SE lambda
+  # as conservative estimate for variable selection
+  # -------------------------
+  x <- data.matrix(df[,-1]) #explan var, glmnet can't use dataframe
+  y <- data.matrix(df[,1]) #dependent var, glmnet can't use dataframe
+  
+  cv.out = cv.glmnet(x, y, alpha = 1, intercept=FALSE) # Fit lasso model on training data
+  lam = cv.out$lambda.1se # Select more conservative lambda for variable selection
+  
+  lasso_coef = predict(cv.out, type = "coefficients", s = lam) # Display coefficients using lambda chosen by CV
+  return(var_retention(lasso_coef, beta))
+}
+
+
+
+beta = beta_1(p=100,s=5)
+df <- simulate(n=100, p=100, rho=0.5, beta=beta, SNR = 1)$df
+x <- data.matrix(df[,-1]) #explan var, glmnet can't use dataframe
+y <- data.matrix(df[,1]) #dependent var, glmnet can't use dataframe
+
+practice.vsurf <- VSURF(x=x, y=y, mtry=100)
+
+summary(practice.vsurf)
+
+plot(practice.vsurf)
